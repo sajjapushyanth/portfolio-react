9c2d5f82cfe3b7ec8c2a6898456c77abc586bd71
sajjapushyanth <sp7173@srmist.edu.in>
2024-04-11 11:01:40 +0530

    Initial commit

---
+++ portfolio/portfolio/src/App.jsx
@@ -0,0 +1,45 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <main>
+      <div className="container"> 
+      <div className="title">
+        <h1 className="title-name">Sajja Pushyanth</h1>
+      </div>
+
+      <div className="about">
+        <h2 className="about-title">About</h2>
+        <p className="about-text">  Hi I am Sajja Pushyanth, optimistic person, quick learner,
+         my hobbies are playing Badminton,watching TV
+         </p>
+      </div>
+
+      <div className="tech-stack">
+          
+          <p className="tech-stack-text">
+            My Tech Stack is Java,Python,SQL,React Js,Git
+          </p>
+
+      </div>
+         
+      <div className="social-media">
+        
+        <a href="https://www.facebook.com/pushy.pushyanth?mibextid=JRoKGi"> Facebook</a>
+        <a href="https://www.instagram.com/pushy02?igsh=MTd6dG82M3E3bWNuMQ=="> Instagram</a>
+        <a href="https://github.com/sajjapushyanth">Github</a>
+        <a href="https://www.linkedin.com/in/pushyanth-sajja-a8576619b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Linkedin</a>
+      
+      </div>
+
+
+      </div>
+      
+    </main>
+  )
+}
+
+export default App
