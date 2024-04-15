77b22e1ca7270a2d48bcc92ce72336a6d53801f2
sajjapushyanth <sp7173@srmist.edu.in>
2024-04-15 15:45:28 +0530

    navbar changes

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Main from './components/Main'
+import Navbar from './components/Navbar'
+function App() {
+  
+
+  return (
+    <main>
+      <Navbar />
+      <hr />
+      <Main />
+    </main>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <div className="sub">
+            
+            <h2>Home</h2>
+            <h2>About</h2>
+            <h2>Contact </h2>
+            </div>
+            <div className='hello-world-container'>
+                <button className='hello-world'>hello World</button>
+
+            </div>
+        </div>
+    )
+}
